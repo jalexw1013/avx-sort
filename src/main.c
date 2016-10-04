6e0628b13682ece4a9c64fb79fa0544b81e7ec4d
Alex Watkins <alex@alexwatkins.co>
2016-10-04 09:10:50 -0400

    Finished avx sort

--- src/main.c
+++ src/main.c
@@ -289,8 +289,8 @@ inline void serialMergeAVX2(vec_t* A, int32_t A_length,
 	__m256i miAi = _mm256_set_epi32(0,0,0,0,0,0,0,0);
 	__m256i miBi = _mm256_set_epi32(0,0,0,0,0,0,0,0);
 
-    while (/*_mm256_cmpgt_epi32(vindexAStop, vindexA)
-        || _mm256_cmpgt_epi32(vindexBStop, vindexB)*/0) {
+    while (_mm256_cmpgt_epi32(vindexAStop, vindexA) != _mm256_set_epi32(1,1,1,1,1,1,1,1)
+        || _mm256_cmpgt_epi32(vindexBStop, vindexB) != _mm256_set_epi32(1,1,1,1,1,1,1,1)) {
         __m256i miAelems = _mm256_i32gather_epi32(A, vindexA, 1);
         __m256i miBelems = _mm256_i32gather_epi32(B, vindexB, 1);
         __m256i micmp   = _mm256_cmpgt_epi32(miBelems, miAelems);
@@ -304,6 +304,147 @@ inline void serialMergeAVX2(vec_t* A, int32_t A_length,
         vindexC = _mm256_add_epi32(vindexC, _mm256_set_epi32(1,1,1,1,1,1,1,1));
         vindexA = _mm256_add_epi32(vindexA, miAi);
         vindexB = _mm256_add_epi32(vindexB, miBi);
+    }
+
+    __int32 a0 = _mm256_extract_epi32(vindexA, 0);
+    __int32 a1 = _mm256_extract_epi32(vindexA, 1);
+    __int32 a2 = _mm256_extract_epi32(vindexA, 2);
+    __int32 a3 = _mm256_extract_epi32(vindexA, 3);
+    __int32 a4 = _mm256_extract_epi32(vindexA, 4);
+    __int32 a5 = _mm256_extract_epi32(vindexA, 5);
+    __int32 a6 = _mm256_extract_epi32(vindexA, 6);
+    __int32 a7 = _mm256_extract_epi32(vindexA, 7);
+
+    __int32 a0Stop = _mm256_extract_epi32(vindexAStop, 0);
+    __int32 a1Stop = _mm256_extract_epi32(vindexAStop, 1);
+    __int32 a2Stop = _mm256_extract_epi32(vindexAStop, 2);
+    __int32 a3Stop = _mm256_extract_epi32(vindexAStop, 3);
+    __int32 a4Stop = _mm256_extract_epi32(vindexAStop, 4);
+    __int32 a5Stop = _mm256_extract_epi32(vindexAStop, 5);
+    __int32 a6Stop = _mm256_extract_epi32(vindexAStop, 6);
+    __int32 a7Stop = _mm256_extract_epi32(vindexAStop, 7);
+
+    __int32 b0 = _mm256_extract_epi32(vindexB, 0);
+    __int32 b1 = _mm256_extract_epi32(vindexB, 1);
+    __int32 b2 = _mm256_extract_epi32(vindexB, 2);
+    __int32 b3 = _mm256_extract_epi32(vindexB, 3);
+    __int32 b4 = _mm256_extract_epi32(vindexB, 4);
+    __int32 b5 = _mm256_extract_epi32(vindexB, 5);
+    __int32 b6 = _mm256_extract_epi32(vindexB, 6);
+    __int32 b7 = _mm256_extract_epi32(vindexB, 7);
+
+    __int32 b0Stop = _mm256_extract_epi32(vindexBStop, 0);
+    __int32 b1Stop = _mm256_extract_epi32(vindexBStop, 1);
+    __int32 b2Stop = _mm256_extract_epi32(vindexBStop, 2);
+    __int32 b3Stop = _mm256_extract_epi32(vindexBStop, 3);
+    __int32 b4Stop = _mm256_extract_epi32(vindexBStop, 4);
+    __int32 b5Stop = _mm256_extract_epi32(vindexBStop, 5);
+    __int32 b6Stop = _mm256_extract_epi32(vindexBStop, 6);
+    __int32 b7Stop = _mm256_extract_epi32(vindexBStop, 7);
+
+    __int32 c0 = _mm256_extract_epi32(vindexC, 0);
+    __int32 c1 = _mm256_extract_epi32(vindexC, 1);
+    __int32 c2 = _mm256_extract_epi32(vindexC, 2);
+    __int32 c3 = _mm256_extract_epi32(vindexC, 3);
+    __int32 c4 = _mm256_extract_epi32(vindexC, 4);
+    __int32 c5 = _mm256_extract_epi32(vindexC, 5);
+    __int32 c6 = _mm256_extract_epi32(vindexC, 6);
+    __int32 c7 = _mm256_extract_epi32(vindexC, 7);
+
+    while (a0 < a0Stop && b0 < b0Stop) {
+        C[c0++] = A[a0] < B[b0] ? A[a0++] : B[b0++];
+    }
+
+    while (a1 < a1Stop && b1 < b1Stop) {
+        C[c1++] = A[a1] < B[b1] ? A[a1++] : B[b1++];
+    }
+
+    while (a2 < a2Stop && b2 < b2Stop) {
+        C[c2++] = A[a2] < B[b2] ? A[a2++] : B[b2++];
+    }
+
+    while (a3 < a3Stop && b3 < b3Stop) {
+        C[c3++] = A[a3] < B[b3] ? A[a3++] : B[b3++];
+    }
+
+    while (a4 < a4Stop && b4 < b4Stop) {
+        C[c4++] = A[a4] < B[b4] ? A[a4++] : B[b4++];
+    }
+
+    while (a5 < a5Stop && b5 < b5Stop) {
+        C[c5++] = A[a5] < B[b5] ? A[a5++] : B[b5++];
+    }
+
+    while (a6 < a6Stop && b6 < b6Stop) {
+        C[c6++] = A[a6] < B[b6] ? A[a6++] : B[b6++];
+    }
+
+    while (a7 < a7Stop && b7 < b7Stop) {
+        C[c7++] = A[a7] < B[b7] ? A[a7++] : B[b7++];
+    }
+
+    while (a0 < a0Stop) {
+        C[c0++] = A[a0++];
+    }
+
+    while (a1 < a1Stop) {
+        C[c1++] = A[a1++];
+    }
+
+    while (a2 < a2Stop) {
+        C[c2++] = A[a2++];
+    }
+
+    while (a3 < a3Stop) {
+        C[c3++] = A[a3++];
+    }
+
+    while (a4 < a4Stop) {
+        C[c4++] = A[a4++];
+    }
+
+    while (a5 < a5Stop) {
+        C[c5++] = A[a5++];
+    }
+
+    while (a6 < a6Stop) {
+        C[c6++] = A[a6++];
+    }
+
+    while (a7 < a7Stop) {
+        C[c7++] = A[a7++];
+    }
+
+    while (b0 < b0Stop) {
+        C[c0++] = B[b0++];
+    }
+
+    while (b1 < b1Stop) {
+        C[c1++] = B[b1++];
+    }
+
+    while (b2 < b2Stop) {
+        C[c2++] = B[b2++];
+    }
+
+    while (b3 < b3Stop) {
+        C[c3++] = B[b3++];
+    }
+
+    while (b4 < b4Stop) {
+        C[c4++] = B[b4++];
+    }
+
+    while (b5 < b5Stop) {
+        C[c5++] = B[b5++];
+    }
+
+    while (b6 < b6Stop) {
+        C[c6++] = B[b6++];
+    }
+
+    while (b7 < b7Stop) {
+        C[c7++] = B[b7++];
     }
 }
 
