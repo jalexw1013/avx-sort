0bc28ab374d280935c0ab59d137e12bb7181efd6
Alex Watkins <alex@alexwatkins.co>
2017-03-13 22:57:59 -0400

    Updated sbatch

--- src/sbatchrun.sh
+++ src/sbatchrun.sh
@@ -13,6 +13,9 @@
 #SBATCH -N 1                     # Total number of nodes requested (16 cores/node)
 #SBATCH -n 68                    # Total number of tasks
 #SBATCH -t 00:00:30              # Run time (hh:mm:ss) - 1.5 hours
+#SBATCH --mail-user=jwatkins45@gatech.edu
+#SBATCH --mail-type=begin  # email me when the job starts
+#SBATCH --mail-type=end    # email me when the job finishes
 
 # Launch merge of different sizes.
 #./merge -A 10000 -B 10000
