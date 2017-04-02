8813c3afdd5ea2c8b2ccab22bc4bb67aa53a2df1
Alex Watkins <alex@alexwatkins.co>
2017-04-01 19:49:37 -0500

    Added results

--- src/sbatchrun.sh
+++ src/sbatchrun.sh
@@ -11,13 +11,13 @@
 #SBATCH -o merge.%j.out          # Specify stdout output file (%j expands to jobId)
 #SBATCH -p development           # Queue name
 #SBATCH -N 1                     # Total number of nodes requested (16 cores/node)
-#SBATCH -n 272                   # Total number of tasks
-#SBATCH -t 00:45:00              # Run time (hh:mm:ss) - 1.5 hours
+#SBATCH -n 68                   # Total number of tasks
+#SBATCH -t 00:05:00              # Run time (hh:mm:ss) - 1.5 hours
 #SBATCH --mail-user=jwatkins45@gatech.edu
 #SBATCH --mail-type=begin  # email me when the job starts
 #SBATCH --mail-type=end    # email me when the job finishes
 
 # Launch merge of different sizes.
-#./merge -A 10000 -B 10000
+./merge -A 10000 -B 10000
 ./merge
-#./merge -A 1000000000 -B 1000000000
+./merge -A 100000000 -B 100000000
