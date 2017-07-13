4d51112149149cac7d273daf3ff6b0afdd034b0f
Watkins <awatkin@amazon.com>
2017-07-13 10:26:51 -0700

    Prepared for local testing

--- src/main.c
+++ src/main.c
@@ -98,7 +98,7 @@ uint32_t  h_ui_A_length                = 500000;
 uint32_t  h_ui_B_length                = 500000;
 uint32_t  h_ui_C_length                = 1000000; //array to put values in
 uint32_t  h_ui_Ct_length               = 1000000; //for unsorted and sorted
-uint32_t  RUNS                         = 100;
+uint32_t  RUNS                         = 50;
 uint32_t  entropy                      = 28;
 uint32_t  OutToFile                    = 0; // 1 if out put to file
 
@@ -106,9 +106,9 @@ uint32_t  OutToFile                    = 0; // 1 if out put to file
 const uint32_t testingEntropies[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40};
 const uint32_t testingEntropiesLength = 40;
 const uint32_t testingSizes[] = {100000, 1000000, 10000000, 100000000};
-const uint32_t testingSizesLength = 4;
-const uint32_t testingThreads[] = {1,2,4,6,8,16,32,36,48,68,72};//*/{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100};
-const uint32_t testingThreadsLength = 11;
+const uint32_t testingSizesLength = 3;
+const uint32_t testingThreads[] = {1,2,4};//*/{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100};
+const uint32_t testingThreadsLength = 3;
 
 // Host Functions
 ////////////////////////////
