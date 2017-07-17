2bffa58ec35f2747c5531bc7355b31bdb5439c65
Watkins <awatkin@amazon.com>
2017-07-17 09:40:42 -0700

    Added timming for debugging

--- src/main.c
+++ src/main.c
@@ -98,7 +98,7 @@ uint32_t  h_ui_A_length                = 500000;
 uint32_t  h_ui_B_length                = 500000;
 uint32_t  h_ui_C_length                = 1000000; //array to put values in
 uint32_t  h_ui_Ct_length               = 1000000; //for unsorted and sorted
-uint32_t  RUNS                         = 50;
+uint32_t  RUNS                         = 1;
 uint32_t  entropy                      = 28;
 uint32_t  OutToFile                    = 0; // 1 if out put to file
 
