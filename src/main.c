7726a41aa42a1558ad75844c28cfab92407fbc8d
Alex Watkins <jwatkins45@gatech.edu>
2018-02-26 12:40:29 -0500

    Prepared for testing, added copyright header

--- src/main.c
+++ src/main.c
@@ -1,3 +1,38 @@
+/*
+  Authors: Alex Watkins - https://github.com/jalexw1013 - http://alexwatkins.co
+           Oded Green - https://github.com/ogreen
+           Other minor authors are noted next to their contributions in the code
+
+         Copyright (c) 2018 Alex Watkins, All rights reserved.
+
+         Redistribution and use in source and binary forms, with or without
+         modification, are permitted provided that the following conditions
+         are met:
+
+         1. Redistributions of source code must retain the above copyright
+         notice, this list of conditions and the following disclaimer.
+
+         2. Redistributions in binary form must reproduce the above copyright
+         notice, this list of conditions and the following disclaimer in the
+         documentation and/or other materials provided with the distribution.
+
+         3. Neither the name of the copyright holder nor the names of its
+         contributors may be used to endorse or promote products derived
+         from this software without specific prior written permission.
+
+         THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+         "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+         LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+         FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+         COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+         INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+         BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+         LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+         CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+         LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+         ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+         POSSIBILITY OF SUCH DAMAGE.
+*/
 #include <stdio.h>
 #include <sys/time.h>
 #include <stdint.h>
@@ -86,7 +121,7 @@ uint32_t  OutToFile                    = 0; // 1 if output to file
 
 uint32_t testingEntropies[] = {28};//{1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31};
 uint32_t testingEntropiesLength = 1;//10;
-uint32_t testingSizes[] = {1073741824};//{1073741824};//{1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824};
+uint32_t testingSizes[] = {268435456};//{1073741824};//{1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824};
 uint32_t testingSizesLength = 1;//11;
 uint32_t testingThreads[] = {2, 4, 8, 16, 32, 64, 128, 256};
 uint32_t testingThreadsLength = 8;
@@ -656,7 +691,7 @@ int main(int argc, char** argv)
                 testAlgo<parallelIterativeMergeSort<avx512SortNoMergePathV2<avx512Merge>, avx512Merge>>("AVX-512 Optimized", false, false, ParallelSort);
                 // testAlgo<ippRadixSort>("IPP Radix", false, false, Sort);
                 // testAlgo<tbbSort>("TBB", false, false, ParallelSort);
-                // testAlgo<haichuanwangSort>("haichuanwang", false, false, ParallelSort);
+                testAlgo<haichuanwangSort>("haichuanwang", false, false, ParallelSort);
                 printf("\n");
             }
         }
